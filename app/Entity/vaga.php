4dfd4d3b218def78d003ee8d4707f678a6c2633c
Lucas Reis <lucasreis_cod@hotmail.com>
2022-05-31 21:13:35 -0300

    Add Metodo responsavel por cadastrar a vaga no DB

---
+++ app/Entity/vaga.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Entity;
+
+class Vaga{
+
+    //Identificação unica da vaga
+    public $id;
+
+    //Titulo da vaga
+    public $titulo;
+
+    //Descrição da vaga(Pode coter HTML)
+    public $descricao;
+
+    //Define se a vaga está ativa
+    public $ativo;
+
+    //Data de publicaçã oda vaga
+
+    public $data;
+    
+
+}
+
--- cadastrar.php
+++ cadastrar.php
@@ -2,10 +2,21 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use \App\Entity\Vaga;
+
 //VALIDAÇÂO
 if(isset($_POST['titulo'], $_POST['descricao'],$_POST['ativo'])){
-    die('Cadastrar');
+    $obVaga = new Vaga;
+
+    $obVaga->titulo = $_POST['titulo'];
+    $obVaga->descricao = $_POST['descricao'];
+    $obVaga->ativo = $_POST['ativo'];
+
+    $obVaga->cadastrar();
 }
+
+
+
 include __DIR__.'/includes/header.php';
 include __DIR__.'/includes/formulario.php';
 include __DIR__.'/includes/footer.php';
